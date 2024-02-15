1fb9f667d8247350758ef17e49754462ace8cd2f
hana <ansgksk97@naver.com>
2024-02-15 16:19:14 +0900

    02.15 정육점 entity 추가

--- src/main/java/com/green/gogiro/GogiroApplication.java
+++ src/main/java/com/green/gogiro/GogiroApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableJpaAuditing
+
 public class GogiroApplication {
 
     public static void main(String[] args) {
