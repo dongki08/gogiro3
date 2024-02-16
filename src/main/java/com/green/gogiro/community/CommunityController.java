6f7865e6c7a1b66d576c9c6b77ab86a9472d58c8
dongki <ehdgusdl0816@gmail.com>
2024-02-16 17:41:45 +0900

    커뮤니티 최종 sawgger코멘트

--- src/main/java/com/green/gogiro/community/CommunityController.java
+++ src/main/java/com/green/gogiro/community/CommunityController.java
@@ -59,37 +59,83 @@ public class CommunityController {
     }
 
     @GetMapping()
-    @Operation(summary = "커뮤니티 리스트", description = "커뮤니티 리스트 처리")
+    @Operation(summary = "커뮤니티 리스트", description = "커뮤니티 리스트 처리<br>" +
+            "--요청데이터<br>page : 페이지처리<br>" +
+            "search : 제목검색<br>" +
+            "filter : 0(작성자순), 1(추천순)<br>" +
+            "--응답데이터<br>" +
+            "iboard : 커뮤니티pk<br>" +
+            "boardNum : 게시글번호<br>" +
+            "iuser : 유저pk<br>" +
+            "writerName : 작성자 이름<br>" +
+            "writerPic : 작성자 프로필 사진<br>" +
+            "title : 제목<br>" +
+            "contents : 내용<br>" +
+            "createdAt : 작성일<br>" +
+            "pics : 사진리스트<br>" +
+            "count : 게시글 총 갯수")
     public List<CommunitySelVo> getCommunity(CommunitySelDto dto) {
         return service.selCommunity(dto);
     }
 
     @GetMapping("{iboard}")
-    @Operation(summary = "커뮤니티 상세보기", description = "커뮤니티 상세보기 처리")
+    @Operation(summary = "커뮤니티 상세보기", description = "커뮤니티 상세보기 처리<br>" +
+            "--요청데이터<br>" +
+            "iboard : 커뮤니티pk<br>" +
+            "--응답데이터<br>" +
+            "iboard : 커뮤니티pk<br>" +
+            "iuser : 유저pk<br>" +
+            "writerName : 작성자 이름<br>" +
+            "writerPic : 작성자 프로필 사진<br>" +
+            "title : 제목<br>" +
+            "contents : 내용<br>" +
+            "createdAt : 작성일<br>" +
+            "pics : 사진리스트<br>" +
+            "be : 이전글<br>" +
+            "af : 다음글<br>" +
+            "comments : 댓글리스트")
     public CommunityDetailVo getDetailCommunity(@PathVariable int iboard) {
         return service.getDetailCommunity(iboard);
     }
 
     @DeleteMapping()
-    @Operation(summary = "커뮤니티 삭제",description = "커뮤니티 삭제 처리")
+    @Operation(summary = "커뮤니티 삭제",description = "커뮤니티 삭제 처리<br>" +
+            "--요청데이터<br>" +
+            "iboard : 커뮤니티pk<br>" +
+            "--응답데이터<br>" +
+            "result : 1(성공), 에러메세지(실패)")
     public ResVo delCommunity(CommunityDelDto dto) {
         return service.delCommunity(dto);
     }
 
     @PostMapping("/comment")
-    @Operation(summary = "커뮤니티 댓글 작성",description = "커뮤니티 댓글 작성 처리")
+    @Operation(summary = "커뮤니티 댓글 작성",description = "커뮤니티 댓글 작성 처리<br>" +
+            "--요청데이터<br>" +
+            "iboard : 커뮤니티pk<br>" +
+            "contents : 댓글내용(1~50자)<br>" +
+            "--응답데이터<br>" +
+            "result : 1(성공), 에러메세지(실패)")
     public ResVo postCommunityComment(@Valid @RequestBody CommunityCommentInsDto dto) {
         return service.postCommunityComment(dto);
     }
 
     @DeleteMapping("/comment")
-    @Operation(summary = "커뮤니티 댓글 삭제",description = "커뮤니티 댓글 삭제 처리")
+    @Operation(summary = "커뮤니티 댓글 삭제",description = "커뮤니티 댓글 삭제 처리<br>" +
+            "--요청데이터<br>" +
+            "icomment : 댓글pk<br>" +
+            "--응답데이터<br>" +
+            "result : 1(성공), 에러메세지(실패)")
     public ResVo delCommunityComment(@RequestBody CommunityCommentDelDto dto) {
         return service.delCommunityComment(dto);
     }
 
     @GetMapping("/fav")
-    @Operation(summary = "좋아요 기능", description = "좋아요 처리(1), 좋아요 삭제(0)")
+    @Operation(summary = "좋아요 기능", description = "좋아요 처리<br>" +
+            "--요청데이터<br>" +
+            "iuser : 유저pk<br>" +
+            "iboard : 커뮤니티pk<br>" +
+            "--응답데이터<br>" +
+            "result : 1(좋아요), 0(좋아요취소)")
     public ResVo favCommunity(@RequestBody CommunityInsFavDto dto) {
         return service.favCommunity(dto);
     }
