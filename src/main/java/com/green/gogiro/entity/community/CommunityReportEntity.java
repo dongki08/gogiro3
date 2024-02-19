efd207af63f682e1e1c741c0232a0d6b8894e617
dongki <ehdgusdl0816@gmail.com>
2024-02-19 18:11:42 +0900

    02-19 커뮤신고, 댓글신고 스웨거설명 작성

--- src/main/java/com/green/gogiro/entity/community/CommunityReportEntity.java
+++ src/main/java/com/green/gogiro/entity/community/CommunityReportEntity.java
@@ -1,12 +1,13 @@
 package com.green.gogiro.entity.community;
 
+import com.green.gogiro.entity.CreatedAtEntity;
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "t_report_category")
-public class CommunityReportCategoryEntity {
+@Table(name = "t_report")
+public class CommunityReportEntity extends CreatedAtEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "BIGINT UNSIGNED")
